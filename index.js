733b39b9161e5dbda06394fe11d9e8da612192c0
conniekang <hqk1121@gmail.com>
2021-09-03 12:16:54 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+let employee = {'name': 'Connie', 'stresstAddress': 'earth'}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, [key]:value};
+}
+
+employee = {'name': 'Connie', 'stresstAddress': 'earth'}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+employee = {'name': 'Connie', 'stresstAddress': 'earth'}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {...employee}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+employee = {'name': 'Connie', 'stresstAddress': 'earth'}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
